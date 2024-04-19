12b85b63cdb079b5fbd8ddfdfdfda5d3da760ef7
QuantumDeity <greasyRooster1@gmail.com>
2024-04-18 20:36:42 -0700

    added text

--- src/main/Render/HUDRender.java
+++ src/main/Render/HUDRender.java
@@ -1,11 +1,13 @@
 package main.Render;
 
 import main.Main;
+import processing.core.PConstants;
 
 public class HUDRender {
     public static void render(){
         Main.app.fill(1);
         Main.app.textSize(16);
-        Main.app.text("FPS: "+Main.app.frameRate,10,10);
+        Main.app.textAlign(PConstants.LEFT);
+        Main.app.text("FPS: "+Main.app.frameRate,5,15);
     }
 }
