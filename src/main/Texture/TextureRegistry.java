8cbf11877d126e75ff735b6d1551af38481212b9
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-19 10:03:28 -0700

    added more textures

--- src/main/Texture/TextureRegistry.java
+++ src/main/Texture/TextureRegistry.java
@@ -1,6 +1,8 @@
 package main.Texture;
 
+import main.Main;
 import main.Texture.TextureTypes.ErrorTexture;
+import main.Texture.TextureTypes.GradientTexture;
 import main.Texture.TextureTypes.ImageTexture;
 import main.Texture.TextureTypes.UVTestTexture;
 
@@ -13,6 +15,7 @@ public class TextureRegistry {
         register(new ErrorTexture());
         register(new UVTestTexture());
         register(new ImageTexture("walls.brick"));
+        register(new GradientTexture(Main.app.color(1, 0.6f, 0.9f),Main.app.color(0.6f, 0.9f, 1)));
     }
     public static void register(Texture texture){
         texture.id = textures.length;
