ffe9e4fcd6ea68b6b57e28368238b85b58678eb3
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-19 09:39:16 -0700

    made textures able to render

--- src/main/Texture/Texture.java
+++ src/main/Texture/Texture.java
@@ -1,7 +1,17 @@
 package main.Texture;
 
+import main.Main;
+
 public abstract class Texture {
     public int getColor(float uv_x,float uv_y){
         return 0;
+    }
+    public void renderTexture(float x,float y,float w,float h,int res){
+        for (int i = 0; i < w; i+=res) {
+            for (int j = 0; j < h; j+=res) {
+                Main.app.fill(getColor(i/w,j/h));
+                Main.app.rect(x,y,res,res);
+            }
+        }
     }
 }
