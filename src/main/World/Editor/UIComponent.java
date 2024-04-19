e33dc6a034553663db9ff13313257663d0df158d
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-19 09:46:45 -0700

    added texture button

--- src/main/World/Editor/UIComponent.java
+++ src/main/World/Editor/UIComponent.java
@@ -6,6 +6,7 @@ import static main.Util.Util.rectRect;
 
 public class UIComponent {
     public float x,y,w,h;
+    public boolean mouseLastFrame = false;
     public UIComponent(float _x, float _y, float _w, float _h){
         x=_x;
         y=_y;
@@ -19,10 +20,11 @@ public class UIComponent {
     public void checkEvents(){
         if(rectRect(x,y,w,h,Main.app.mouseX,Main.app.mouseY,1,1)){
             onHover();
-            if(Main.app.mousePressed){
+            if(Main.app.mousePressed&&!mouseLastFrame){
                 onPress();
             }
         }
+        mouseLastFrame = Main.app.mousePressed;
     }
     public void render(){
         Main.app.rect(x,y,w,h);
