3165e572cb6a86006daae132ed7dff51549714cc
QuantumDeity <greasyRooster1@gmail.com>
2024-04-21 11:45:08 -0700

    render SpawnPoint

--- src/main/World/SpawnPoint.java
+++ src/main/World/SpawnPoint.java
@@ -1,7 +1,13 @@
 package main.World;
 
+import main.Main;
 import main.Util.Point;
 
 public class SpawnPoint {
     public Point position;
+    public void render(){
+        Main.app.fill(1,0,1);
+        Main.app.noStroke();
+        Main.app.ellipse(position.x,position.y,5,5);
+    }
 }
