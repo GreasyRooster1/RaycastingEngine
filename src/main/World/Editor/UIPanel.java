d727fd42913863b9c5c94a8e76b383dc18e7db50
QuantumDeity <greasyRooster1@gmail.com>
2024-04-23 21:21:41 -0700

    added panels

---
+++ src/main/World/Editor/UIPanel.java
@@ -0,0 +1,49 @@
+package main.World.Editor;
+
+import main.Main;
+
+import javax.swing.*;
+
+public class UIPanel extends UIComponent {
+    public boolean active = false;
+    public UIComponent[] components;
+
+    public UIPanel() {
+        super(0,0, Main.app.width, Main.app.height);
+    }
+
+    public void open(){
+        active = true;
+        onOpen();
+    }
+    public void close(){
+        active = false;
+        onClose();
+    }
+
+    public void update(){
+        if(!active) return;
+        renderBackground();
+        updateComponents();
+        checkEvents();
+    }
+
+    public void renderBackground(){
+        Main.app.noStroke();
+        Main.app.fill(.5f,.5f);
+        Main.app.rect(x,y,w,h);
+    }
+
+    public void updateComponents(){
+        for(UIComponent component : components){
+            component.update();
+        }
+    }
+
+    public void onOpen(){
+
+    }
+    public void onClose(){
+
+    }
+}
