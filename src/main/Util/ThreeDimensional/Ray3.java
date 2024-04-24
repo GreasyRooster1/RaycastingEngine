114a7b7e802bf48041dd544afa00050a7764032c
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-24 14:46:42 -0700

    construct ray3d

--- src/main/Util/ThreeDimensional/Ray3.java
+++ src/main/Util/ThreeDimensional/Ray3.java
@@ -3,4 +3,8 @@ package main.Util.ThreeDimensional;
 public class Ray3 {
     public Point3 origin;
     public Vec3 direction;
+    Ray3(Point3 origin,Vec3 direction){
+        this.origin = origin;
+        this.direction = direction;
+    }
 }
