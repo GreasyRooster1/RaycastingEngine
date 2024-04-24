337cef4009388c266bbeee39f6ad68592dda4019
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-24 14:52:27 -0700

    fix transparent fade

--- src/main/Render/Renderer.java
+++ src/main/Render/Renderer.java
@@ -76,11 +76,11 @@ public class Renderer {
             float uv_y = i/segCount;
 
             int color = wall.texture.getColor(wall,uv_x,uv_y);
-            if(ray.mag>=fogDistance){
-                color = Main.app.lerpColor(color,fogColor, 1-((maxViewDistance-ray.mag)/(maxViewDistance-fogDistance)));
-            }
             if(wall.texture.isTransparent&&color==0){
                 continue;
+            }
+            if(ray.mag>=fogDistance){
+                color = Main.app.lerpColor(color,fogColor, 1-((maxViewDistance-ray.mag)/(maxViewDistance-fogDistance)));
             }
             renderSegment(color,x,i,height,segRatio);
         }
