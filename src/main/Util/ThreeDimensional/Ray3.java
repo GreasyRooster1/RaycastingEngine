4e34912d872a1da13ca82bad3bdc96681bee8467
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-24 14:45:06 -0700

    more 3d utils

---
+++ src/main/Util/ThreeDimensional/Ray3.java
@@ -0,0 +1,6 @@
+package main.Util.ThreeDimensional;
+
+public class Ray3 {
+    public Point3 origin;
+    public Vec3 direction;
+}
---
+++ src/main/Util/ThreeDimensional/Vec3.java
@@ -0,0 +1,10 @@
+package main.Util.ThreeDimensional;
+
+public class Vec3 {
+    public float x,y,z;
+    Vec3(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
