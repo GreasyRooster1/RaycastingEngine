3dbf1c30f8d519877fa9fba1e7084bb86adb148a
greasyRooster1 <GreasyRooster1@gmail.com>
2024-04-24 09:42:19 -0700

    added generated texture buttons

--- src/main/Editor/Components/Buttons/TextureButtons/SingleTextureButton.java
+++ src/main/Editor/Components/Buttons/TextureButtons/SingleTextureButton.java
@@ -7,13 +7,18 @@ import main.Texture.TextureRegistry;
 
 public class SingleTextureButton extends UIComponent {
     public int textureId = 0;
-    public SingleTextureButton(float _x, float _y, float _w, float _h) {
+    public SingleTextureButton(float _x, float _y, float _w, float _h, int i) {
         super(_x, _y, _w, _h);
+        textureId = i;
     }
 
     @Override
     public void render() {
-        Main.app.fill(0.75f);
+        if(textureId==MapEditor.textureId){
+            Main.app.fill(0.5f);
+        }else {
+            Main.app.fill(0.75f);
+        }
         Main.app.rect(x,y,w,h,15);
         TextureRegistry.get(textureId).renderTexture(x+15,y+15,w-30,h-30,1);
     }
--- src/main/Editor/Components/Panels/TexturePanel.java
+++ src/main/Editor/Components/Panels/TexturePanel.java
@@ -1,10 +1,16 @@
 package main.Editor.Components.Panels;
 
+import main.Editor.Components.Buttons.TextureButtons.SingleTextureButton;
 import main.Editor.Components.Buttons.TextureButtons.TextureButton;
 import main.Editor.Components.TextButton;
 import main.Editor.UIPanel;
+import main.Texture.TextureRegistry;
+
+import static processing.core.PApplet.floor;
 
 public class TexturePanel extends UIPanel {
+    public float buttonSize = 50;
+    public float buttonMargin = 10;
     public TexturePanel() {
     }
 
@@ -16,6 +22,14 @@ public class TexturePanel extends UIPanel {
     }
 
     public void createComponents(){
-        addComponent(new TextureButton(10,10,90,90));
+        //create main texture button
+        addComponent(new TextureButton(410,310,90,90));
+
+        //create all the generated ones
+        for(int i=0;i<TextureRegistry.textures.length;i++) {
+            float x = buttonMargin+((i%5)*(buttonSize+buttonMargin));
+            float y = floor(i/5f)*(buttonSize+buttonMargin);
+            addComponent(new SingleTextureButton(x, y, buttonSize, buttonSize, i));
+        }
     }
 }
