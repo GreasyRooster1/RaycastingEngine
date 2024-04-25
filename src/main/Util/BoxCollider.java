d3d42d37488dca22e59e264c7ba4671f72dd3e82
QuantumDeity <greasyRooster1@gmail.com>
2024-04-25 16:45:54 -0700

    render collider

--- src/main/Util/BoxCollider.java
+++ src/main/Util/BoxCollider.java
@@ -1,5 +1,7 @@
 package main.Util;
 
+import main.Main;
+
 import static main.Util.Util.lineLine;
 import static main.Util.Util.lineRect;
 
@@ -11,6 +13,13 @@ public class BoxCollider {
         this.width = width;
         this.height = height;
     }
+
+    public void render(float r,float g,float b) {
+        Main.app.stroke(r,g,b);
+        Main.app.noFill();
+        Main.app.rect(x,y,width,height);
+    }
+
     public boolean checkCollisionLine(float x1,float y1,float x2,float y2){
         return lineRect(x1,y1,x2,y2,x,y,width,height);
     }
