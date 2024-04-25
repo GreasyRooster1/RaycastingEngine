32f34d207de58d9b47bfb20dc32d77184cc40aec
QuantumDeity <greasyRooster1@gmail.com>
2024-04-25 16:42:37 -0700

    added box collider

---
+++ src/main/Util/BoxCollider.java
@@ -0,0 +1,17 @@
+package main.Util;
+
+import static main.Util.Util.lineLine;
+import static main.Util.Util.lineRect;
+
+public class BoxCollider {
+    public float x,y,width,height;
+    public BoxCollider(float x, float y, float width, float height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+    public boolean checkCollisionLine(float x1,float y1,float x2,float y2){
+        return lineRect(x1,y1,x2,y2,x,y,width,height);
+    }
+}
