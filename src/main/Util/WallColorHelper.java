2ac01cfe41744ac2eb45952aa3935dbb6c9255d5
QuantumDeity <greasyRooster1@gmail.com>
2024-04-24 17:06:37 -0700

    error colors

--- src/main/Util/WallColorHelper.java
+++ src/main/Util/WallColorHelper.java
@@ -6,7 +6,12 @@ import static processing.core.PApplet.append;
 
 public class WallColorHelper {
     public static int[] wallColors = {};
+
     public static int getWallColor(int id) {
+        if(wallColors.length==0){
+            wallColors = append(wallColors,Main.app.color(1,0,1));
+            wallColors = append(wallColors,Main.app.color(1,1,0));
+        }
         while(wallColors.length <= id){
             wallColors = append(wallColors,Main.app.randomColor());
         }
