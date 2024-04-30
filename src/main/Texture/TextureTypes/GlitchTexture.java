4a51a98c831b9ffb8ee708fd4e0c3275b0c1072e
QuantumDeity <greasyRooster1@gmail.com>
2024-04-29 18:23:51 -0700

    cool glitch effect

--- src/main/Texture/TextureTypes/GlitchTexture.java
+++ src/main/Texture/TextureTypes/GlitchTexture.java
@@ -18,12 +18,16 @@ public class GlitchTexture extends Texture {
     public int getColorFromWall(Wall wall, float uv_x, float uv_y) {
         float x = lerp(wall.x1,wall.x2,uv_x);
         float y = lerp(wall.y1,wall.y2,uv_y);
-        float effectIntensity = (100-dist(x,y,Main.app.player.x,Main.app.player.y))/100;
+        float effectIntensity = (100-dist(x,y,Main.app.player.x,Main.app.player.y))/100 + Main.app.random(-.1f,.1f);
         if(effectIntensity<0.01f){
             return 0;
         }
-        PImage randomTexture = AssetLoader.assets[Main.app.frameCount%AssetLoader.assets.length].image;
+        PImage randomTexture = AssetLoader.assets[Main.app.frameCount%9].image;
         int color = randomTexture.get((int) (uv_x*randomTexture.width), (int) (uv_y*randomTexture.height));
-        float r = Main.app.red(color);
+        float randomness = effectIntensity*.4f;
+        float r = Main.app.red(color)+Main.app.random(-randomness,randomness);
+        float g = Main.app.green(color)+Main.app.random(-randomness,randomness);
+        float b = Main.app.blue(color)+Main.app.random(-randomness,randomness);
+        return Main.app.color(r,g,b,effectIntensity);
     }
 }
